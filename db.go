3c117e5943479cc112207caa37a3d3319fa9a06a
funnythingz <funnythingz69+github@gmail.com>
2015-01-29 01:08:57 +0900

    database config

--- db.go
+++ db.go
@@ -3,12 +3,27 @@ package main
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	_ "log"
+	_ "reflect"
 )
 
 var dbmap gorm.DB
 
 func DbConnect() {
-	dbmap, _ = gorm.Open("mysql", "hoge:hoge@/gogogo_development?charset=utf8&parseTime=True")
+	m := make(map[interface{}]interface{})
+	config, _ := ioutil.ReadFile("database.yml")
+	err := yaml.Unmarshal([]byte(config), &m)
+	check(err)
+
+	adapter := m["development"].(map[interface{}]interface{})["adapter"].(string)
+	encoding := m["development"].(map[interface{}]interface{})["encoding"].(string)
+	username := m["development"].(map[interface{}]interface{})["username"].(string)
+	password := m["development"].(map[interface{}]interface{})["password"].(string)
+	database := m["development"].(map[interface{}]interface{})["database"].(string)
+
+	dbmap, _ = gorm.Open(adapter, username+":"+password+"@/"+database+"?charset="+encoding+"&parseTime=True")
 
 	dbmap.DB()
 
