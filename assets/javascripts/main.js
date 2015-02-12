d7b398b3c4c6cd322202645db5a5c6441041ddfb
funnythingz <funnythingz69+github@gmail.com>
2015-02-13 00:20:48 +0900

    Fixed Themes

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,2 +1,9 @@
 //= require javascripts/jquery-2.1.3.min
 //= require javascripts/bootstrap.min
+
+$(function() {
+  $('body').addClass('theme-' + $('#select-themes input').val());
+  $('#select-themes label').on('click', function() {
+    $('body').removeClass().addClass('theme-' + $('input', this).val());
+  });
+});
