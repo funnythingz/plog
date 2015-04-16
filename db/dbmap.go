ac9c23dc1f98e42abb5216e65a29e6a2fc55005d
funnythingz <funnythingz69+github@gmail.com>
2015-04-16 17:59:29 +0900

    use host Dbmap

--- db/dbmap.go
+++ db/dbmap.go
@@ -12,12 +12,15 @@ var Dbmap gorm.DB
 
 func DbOpen(
 	adapter string,
+	host string,
 	username string,
 	password string,
 	database string,
-	encoding string) {
+	encoding string,
+) {
 
-	Dbmap, _ = gorm.Open(adapter, username+":"+password+"@/"+database+"?charset="+encoding+"&parseTime=True")
+	dataSourceName := username + ":" + password + "@tcp(" + host + ":3306)/" + database + "?charset=" + encoding + "&parseTime=True"
+	Dbmap, _ = gorm.Open(adapter, dataSourceName)
 
 	Dbmap.DB()
 
@@ -29,14 +32,19 @@ func DbOpen(
 }
 
 type env struct {
+	Host     string
+	Username string
+	Password string
 	Database string
 }
 
 type connection struct {
 	Adapter  string
 	Encoding string
+	Host     string
 	Username string
 	Password string
+	Database string
 }
 
 type Config struct {
@@ -56,11 +64,44 @@ func DbConfig() Config {
 func DbConnect(env string) {
 	config := DbConfig()
 
+	var (
+		dbEnv = config.Databases[env]
+
+		host = func() string {
+			if dbEnv.Host != "" {
+				return dbEnv.Host
+			}
+			return config.Connection.Host
+		}()
+
+		username = func() string {
+			if dbEnv.Username != "" {
+				return dbEnv.Username
+			}
+			return config.Connection.Username
+		}()
+
+		password = func() string {
+			if dbEnv.Password != "" {
+				return dbEnv.Password
+			}
+			return config.Connection.Password
+		}()
+
+		database = func() string {
+			if dbEnv.Database != "" {
+				return dbEnv.Database
+			}
+			return config.Connection.Database
+		}()
+	)
+
 	DbOpen(
 		config.Connection.Adapter,
-		config.Connection.Username,
-		config.Connection.Password,
-		config.Databases[env].Database,
+		host,
+		username,
+		password,
+		database,
 		config.Connection.Encoding,
 	)
 }
